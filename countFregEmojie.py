73f0b0c1b4c8d5391094ce357f723f5bd7cbc066
kibambe <69826863+kibambe-0167@users.noreply.github.com>
2022-10-13 07:30:49 +0200

    done with emojie descriptions

--- countFregEmojie.py
+++ countFregEmojie.py
@@ -1,7 +1,15 @@
+''' 
+get freq of emojies.
+
+outout file is: emojie.txt
+'''
 import json
 import re
+from emoji_translate.emoji_translate import Translator
 from commonSubroutines import write
 
+
+emo = Translator(exact_match_only=False, randomize=True)
 FILENAME = "clean_tweets.txt"
 
 # read data line-by-line
@@ -21,7 +29,7 @@ def read():
 # clean
 def process( data ):
   data = data.lower()
-  data = re.sub("[a-z0-9\s,.-|='!\":;]", "", data )
+  data = re.sub("[a-z0-9\s,.-|='!\":;+-]", "", data )
   data = " ".join( data.split() )
   return data 
 
@@ -33,12 +41,23 @@ def freq( data ):
     else: d[i] = 1
   return d
 
+# assign meaning to imojie
+def emoDef( data ):
+  d = {}
+  for k, v in data.items():
+    m = emo.demojify(k)
+    d[k] = [ v, m ]
+  return d
+    
+
 
 
 data = read()
 data = process( data )
 # print( data )
 data = freq( data )
+# print( data )
+data = emoDef( data )
 # print( data )
 data = str( data )
 write("emojies.txt", data )
